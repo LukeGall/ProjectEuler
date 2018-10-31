18f7a0a1fab8a3973cede86998d7abf723cb505f
LukeGall <44098963+LukeGall@users.noreply.github.com>
2018-10-31 07:07:57 +0000

    Finshed prob12

--- src/euler10to19/Euler12.java
+++ src/euler10to19/Euler12.java
@@ -5,21 +5,37 @@ import java.util.HashMap;
 
 public class Euler12 {
 	
-	private static int getTriNums(int n) {
+	private static int getTriNum(int n) {
 		return n*(n+1)/2;
 	}
 	
-	private static void prob12(int size) {
+	private static void prob12() {
+		int triNum=1;
+		int count=1;
+		int fact=0;
 		
+		while (fact<500) {
+			triNum=getTriNum(count);
+			fact=factors(triNum);
+			count++;
+		}
+		System.out.println(triNum);
 	}
 	
 	private static int factors(int num) {
-		if (num%2==0) num=num/2;
-		int factors=1;
+		int factors=0;
+		int sqrt = (int) Math.sqrt(num);
+		for(int i=1;i<sqrt;i++) {
+			if(num % i == 0) factors+=2;
+		}
+		if(sqrt*sqrt==num) {
+			factors--;
+		}
+		return factors;
 		
 	}
 	
 	public static void main(String[] args) {
-		prob12(5000);
+		prob12();
 	}
 }
